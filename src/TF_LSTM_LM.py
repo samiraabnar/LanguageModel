ef1fa83cc192cf4bad2a0bbd70d20f7803710d74
Samira <samira.abnar@gmail.com>
2016-07-01 15:47:16 +0200

    Synching

--- src/TF_LSTM_LM.py
+++ src/TF_LSTM_LM.py
@@ -1,6 +1,49 @@
 import tensorflow as tf
+from tensorflow.models.rnn import rnn, rnn_cell
 
-class TF_LSTM_LM(object):
+import numpy as np
+
+class TF_LSTM(object):
     def __init__(self):
-        lstm = rnn_cell.BasicLSTMCell(100)
-        state = tf.zeros([batch_size, lstm.state_size])
+        self.layers = {}
+
+    def build_model(self):
+        # tf Graph input
+        x = tf.placeholder("float", [None, n_steps, n_input])
+        y = tf.placeholder("float", [None, n_classes])
+
+        # Define weights
+        weights = {
+            'out': tf.Variable(tf.random_normal([n_hidden, n_classes]))
+        }
+        biases = {
+            'out': tf.Variable(tf.random_normal([n_classes]))
+        }
+
+        self.layers["LSTM"] = rnn_cell(10,forget_bias=1.0)
+        self.outputs,self.states = rnn.rnn(self.layers["LSTM"],self.x, dtype=tf.float32)
+
+
+if __name__ == '__main__':
+    num_units = 3
+    input_size = 5
+    batch_size = 2
+    max_length = 8
+    with test_session(use_gpu=use_gpu, graph=tf.Graph()) as sess:
+        initializer = tf.random_uniform_initializer(-0.01, 0.01, seed=self._seed)
+        cell = tf.nn.rnn_cell.LSTMCell(
+            num_units, use_peepholes=True, cell_clip=0.0, initializer=initializer)
+        inputs = max_length * [
+            tf.placeholder(tf.float32, shape=(batch_size, input_size))]
+        outputs, _ = tf.nn.rnn(cell, inputs, dtype=tf.float32)
+        assertEqual(len(outputs), len(inputs))
+        for out in outputs:
+            self.assertEqual(out.get_shape().as_list(), [batch_size, num_units])
+
+        tf.initialize_all_variables().run()
+        input_value = np.random.randn(batch_size, input_size)
+        values = sess.run(outputs, feed_dict={inputs[0]: input_value})
+
+    for value in values:
+        # if cell c is clipped to 0, tanh(c) = 0 => m==0
+        assertAllEqual(value, np.zeros((batch_size, num_units)))
--- src/Theano_LSTM_LM.py
+++ src/Theano_LSTM_LM.py
@@ -16,10 +16,13 @@ from Util.util.data.DataPrep import *
 from Util.util.file.FileUtil import *
 from Util.util.nnet.LearningAlgorithms import *
 
+import itertools
+from six.moves import cPickle
+
 
 class Theano_LSTM_LM(object):
 
-    def __init__(self,input_dim,hidden_dims,output_dim, learning_rate=0.01):
+    def __init__(self,input_dim,hidden_dims,output_dim, learning_rate=0.1):
         self.input_dim = input_dim
         self.hidden_dims = hidden_dims
         self.output_dim = output_dim
@@ -39,14 +42,16 @@ class Theano_LSTM_LM(object):
                                              input_dim=self.input_dim,
                                              output_dim=self.hidden_dims[0],
                                              outer_output_dim=self.output_dim,
-                                             random_state=self.random_state,layer_id="_0")
+                                             random_state=self.random_state,layer_id="_0",bptt_truncate=5)
 
         params = self.layers[0].params
-        cost = T.sum(T.sqrt(T.sum((self.layers[0].hidden_state - next_x) ** 2,axis=1)))
+        params += self.layers[0].output_params
+        cost = T.sum(T.nnet.categorical_crossentropy(self.layers[0].output,next_x))
+
         grads = T.grad(cost,params)
         updates = [(param_i,param_i - self.learning_rate * grad_i) for param_i,grad_i in zip(params,grads)]
         self.learning_step = theano.function([x,next_x],cost,updates=updates)
-        next_gen = T.argmax(self.layers[0].hidden_state)
+        next_gen = T.argmax(self.layers[0].output,axis=1)
         self.predict = theano.function([x],[next_gen])
 
 
@@ -57,48 +62,89 @@ class Theano_LSTM_LM(object):
 
         tokenized_sentences_with_labels = []
         for sent in data:
-            tokenized = nltk.tokenize(sent)
+            tokenized = nltk.tokenize(sent.lower())
             tokenized_sentences_with_labels.append((int(tokenized[0]), tokenized[1:]))
 
 
 
 
 if __name__ == '__main__':
+    sys.setrecursionlimit(10000)
+
     with open("../../LSTM/data/sentiment/trainsentence_and_label_binary.txt", 'r') as filedata:
         data = filedata.readlines()
     char_list = []
-    tokenized_sentences_with_labels = []
+    sentences_with_labels = []
     for sent in data:
-        tokenized_sentences_with_labels.append((int(sent[0]), sent[1:]))
-        for s in sent[1:]:
-            if s not in char_list:
-                char_list.append(s)
-    print(char_list)
-    dic = {}
-    for i in np.arange(len(char_list)):
-        dic[char_list[i]] = i
+        sent = sent.lower()
+        sentences_with_labels.append((int(sent[0]), sent[1:]))
 
-    one_hot_vector = np.eye(len(char_list))
-    one_hot_sents = []
-    for sent in tokenized_sentences_with_labels:
-        one_hot_sent = [ one_hot_vector[dic[c]] for c in sent[1]]
-        one_hot_sents.append(one_hot_sent)
+    tokenized_sentences = [ nltk.word_tokenize(sent[1]) for sent in sentences_with_labels]
 
-    model = Theano_LSTM_LM(len(char_list),[len(char_list)],1)
-    for sent in one_hot_sent:
-        forward_sent = sent[1:]
-        model.learning_step(np.asarray(sent[0:len(sent) - 1],np.float32),np.asarray(sent[1:],np.float32))
+    # Count the word frequencies
+    word_freq = nltk.FreqDist(itertools.chain(*tokenized_sentences))
 
-    first_char = char_list[np.random.randint(0,len(char_list))]
+    unknown_token = 'UNK'
+    vocabulary_size = 4000
+    # Get the most common words and build index_to_word and word_to_index vectors
+    vocab = word_freq.most_common(vocabulary_size - 1)
+    index_to_word = [x[0] for x in vocab]
+    index_to_word.append(unknown_token)
+    word_to_index = dict([(w, i) for i, w in enumerate(index_to_word)])
 
-    current_char = [first_char]
-    sent += current_char[0]
+    print("Using vocabulary size %d." % vocabulary_size)
+    print("The least frequent word in our vocabulary is '%s' and appeared %d times." % (vocab[-1][0], vocab[-1][1]))
 
-    while current_char not in ['.','!','?']:
-        current_char = char_list[model.predict(current_char)]
-        sent += current_char[0]
-        print(sent)
 
+    # Replace all words not in our vocabulary with the unknown token
+    for i, sent in enumerate(tokenized_sentences):
+        tokenized_sentences[i] = [w if w in word_to_index else unknown_token for w in sent]
+
+    print("\nExample sentence: '%s'" % sentences_with_labels[0][1])
+    print("\nExample sentence after Pre-processing: '%s'" % tokenized_sentences[0])
+
+    # Create the training data
+    X_train = np.asarray([[word_to_index[w] for w in sent[:-1]] for sent in tokenized_sentences])
+    y_train = np.asarray([[word_to_index[w] for w in sent[1:]] for sent in tokenized_sentences])
+
+
+
+
+    model = Theano_LSTM_LM(vocabulary_size,[100],vocabulary_size)
+    onehot = np.eye(vocabulary_size)
+
+    for e in np.arange(1,100):
+        iter = 0
+        for i in np.random.permutation(len(X_train)):
+            sent = [ onehot[w] for w in X_train[i]]
+            f_sent = [ onehot[w] for w in y_train[i]]
+            cost = model.learning_step(np.asarray(sent,np.float32),
+                                np.asarray(f_sent,np.float32))
+            iter += 1
+            print(str(iter)+": "+str(cost))
+
+
+        for i in np.arange(1):
+            first_word = np.random.randint(0,vocabulary_size - 1)
+
+            current_input = [first_word]
+            sent = index_to_word[current_input[0]]
+
+            l = 0
+            while sent[-1] not in ['.','!','?'] and l < 100:
+                #print(current_input)
+                p_word = model.predict(np.asarray([onehot[cw] for cw in current_input],dtype=np.float32))
+                #print(p_char[0])
+                #print(p_char[0][-1])
+                current_input.append(p_word[0][-1])
+                sent += " " + index_to_word[p_word[0][-1]]
+                l += 1
+
+            print(sent)
+
+        modelfile = "LM"+str(iter)
+        with open(modelfile, "wb") as f:
+            cPickle.dump(model, f, protocol=cPickle.HIGHEST_PROTOCOL)
 
 
 
